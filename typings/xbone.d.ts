d529234a8a7c8b4580453849920d5698183e7a54
kanno <812137533@qq.com>
2022-04-27 19:26:00 +0800

    feat: 补全dts

--- typings/xbone.d.ts
+++ typings/xbone.d.ts
@@ -1,3 +1,5 @@
+import Victor from 'victor'
+
 export type XboneClientScene = 'web' | 'ohos'
 
 export type XboneCoordinate = 'x' | 'y'
@@ -24,7 +26,68 @@ export interface XboneOPtions {
 }
 
 declare class Xbone {
-  constructor(option: any)
+  constructor(option: XboneOPtions)
+  init(): void
+  /**
+   * 播放动画
+   */
+  play(): void
+  /**
+   * 暂停动画
+   */
+  pause(): void
+  animeClear(): void
+  checkBoneAnime(): any[]
+  getAnimeGridList(): any[]
+  reloadImgDraw(): void
+  drawImageItem(
+    img: ImageData,
+    imageScale: number,
+    img_width?: null | number,
+    img_height?: null | number
+  ): void
+  gridDataRelative(index: number, griding_info?: any): void
+  drawGridItem(
+    info: Record<
+      | 'img'
+      | 'img_point'
+      | 'gridingPoint'
+      | 'triangleList'
+      | 'img_width'
+      | 'img_height'
+      | 'boneList'
+      | 'bone_weight'
+      | 'boneMoved_angle',
+      any
+    >
+  ): void
+  getParentBoneRotatePoint(
+    point: unknown,
+    boneList: unknown,
+    b_idx: unknown,
+    img_point: unknown,
+    angleList: unknown[]
+  ): Victor
+  replaceRotatePoint(
+    angle: unknown,
+    bone: unknown,
+    point: unknown,
+    img_point: unknown
+  ): Victor
+  getTransformMatrix(old_t, new_t): any[]
+  getItemList(numList: unknown[], m, n): any[]
+  setImgPointList(imgPointList: unknown[]): void
+  setScaleList(scale_list): void
+  setGrid_list(grid_list): void
+  setBoneMoved_angle(info, index, boneIndex, angle): any
+  getPointByOffsetAndAngle(point, offset, angle, dl): Record<'x' | 'y', number>
+  subBoneLinkAge(info, boneIndex): any
+  getRotateByMouseMove(orgP, startP, endP): number
+  pow(x: number): number
+  diffPow(x: number, y: number): number
+  replaceRotateBonePoint(angle, bone, point, img_point): Victor
+  getTwoPointAngle(bone): number
+  clearCanvas(): void
 }
 
-export default Xbone
+export { Xbone }
